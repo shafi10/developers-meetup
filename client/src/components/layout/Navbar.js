2fb0723268a0a04481f6778967c5948b3292e494
shafi10 <shafiswe@gmail.com>
2020-08-21 14:01:50 +0600

    make it better

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,7 +7,7 @@ import {logout } from '../../actions/auth'
 const Navbar = ({ logout, auth: {isAuthenticated, loding}}) => {
   return (
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-<Link className="navbar-brand" to="/">Developers Join</Link>
+<Link className="navbar-brand" to="/">Developers Meet Up</Link>
 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
   <span className="navbar-toggler-icon"></span>
 </button>
